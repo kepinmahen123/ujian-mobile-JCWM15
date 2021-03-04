3826179f2120cb935417e30fed140bd7c1d1d20c
kepinmahen <kepinmahen@gmail.com>
2021-03-04 17:29:37 +0700

    'ujianMobileKevin'

---
+++ src/screen/navigation/HomeNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Icon} from 'react-native-elements';
+import ProductStack from './ProductStack';
+import {useSelector} from 'react-redux';
+
+const Tab = createBottomTabNavigator();
+
+const HomeNav = () => {
+  
+}
